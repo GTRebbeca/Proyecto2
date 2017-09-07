77612c3975f7fee82b9ad27bb0404f80d79e100b
GTRebbeca <rebbecagt@gmail.com>
2017-09-07 16:28:09 -0500

    Calendario

---
+++ calendario/demos/config.php
@@ -0,0 +1,7 @@
+<?php
+define("CLIENT_ID", "3MVG9g9rbsTkKnAXC68C6UMFLGJYSTKkj_acSWQKDMmo_lRvlzI94vPXhomN6d0TkSYhhowkENyED2nY2E4_2");
+define("CLIENT_SECRET", "3037552499854743451");
+define("REDIRECT_URI", "https://localhost/calendario/demos/oauth_callback.php");
+define("LOGIN_URI", "https://login.salesforce.com");
+define("CLAVE", "grupodos1238iJMf7837BFOfC5RXscI7PYJ");
+define("USERNAME", "Admin1@MobileJS.com");
---
+++ calendario/demos/insert2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cuentas</title>
+</head>
+<body> 
+
+<?php
+session_start();
+
+$access_token = $_SESSION['access_token'];
+$instance_url = $_SESSION['instance_url'];
+$name= "Rebbeca";
+$lastname= "Goncalves";
+//$query = "INSERT Id, Name FROM Account";
+$content = json_encode(array("FirstName" => $name, "LastName" => $lastname));
+$url = "$instance_url/services/data/v20.0/sobjects/Contact/";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER,
+        array("Authorization: OAuth $access_token", "Content-type: application/json"));
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+
+$json_response = curl_exec($curl);
+
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+if ( $status != 201 ) {
+    die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+}
+
+echo "HTTP status $status creating account<br/><br/>";
+
+curl_close($curl);
+
+$response = json_decode($json_response, true);
+
+$id = $response["id"];
+
+echo "New record id $id<br/><br/>";
+
+return $id;
+
+?>
+</body>   
+   
+</html>       
